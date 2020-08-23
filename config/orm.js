78c70fe8da0c3e8f33cc491c1163cb68a98b3fa2
Sweat <upsidedownplatypus@gmail.com>
2020-08-22 23:46:40 -0400

    app running

--- config/orm.js
+++ config/orm.js
@@ -1,45 +1,91 @@
-const connection = require('./connection');
+var connection = require("../config/connection.js");
 
-const orm = {
-  selectAll: function (cb) {
-    connection.query('SELECT * FROM burgers_db', function (err, data) {
-      if (err) cb(err, null);
-      cb(null, data);
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+
+  return arr.toString();
+}
+
+var orm = {
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM burgers;";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
     });
   },
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO burgers";
 
-  selectAllBy: function (condition, value, cb) {
-    const sqlQuery = `SELECT * FROM burgers_db WHERE ${condition} = ${value}`;
-    connection.query(sqlQuery, function (err, data) {
-      if (err) cb(err, null);
-      cb(null, data);
-    });
-  },
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
 
-  insertOne: function (burgerName, cb) {
-    const sqlQuery = `INSERT INTO burgers_db(burger_name) VALUES('${burgerName}')`;
-    connection.query(sqlQuery, function (err, data) {
-      if (err) cb(err, null);
-      cb(null, data);
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
     });
   },
   
-  deleteOne: function (id, cb) {
-    const sqlQuery = `DELETE FROM burgers_db WHERE id = ${id}`;
-    connection.query(sqlQuery, function (err, data) {
-      if (err) cb(err, null);
-      cb(null, data);
+  update: function(objColVals, condition, cb) {
+    var queryString = "UPDATE burgers";
+    queryString += " SET ";
+    queryString += objColVals;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
     });
   },
+  delete: function(table, condition, cb) {
+    var queryString = "DELETE FROM burgers";
+    queryString += " WHERE ";
+    queryString += condition;
 
-  updateOne: function (condition, id, cb) {
-    const sqlQuery = `UPDATE burgers_db SET is_favorite = ${condition} WHERE id = ${id}`;
-    connection.query(sqlQuery, function (err, data) {
-      if (err) cb(err, null);
-      cb(null, data);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
     });
-  },
-
+  }
 };
 
 module.exports = orm;
